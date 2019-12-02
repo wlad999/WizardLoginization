a997264085f20f4d25737713776ca3c65ac5e785
ваше имяwlad999 <wladmail25@gmeil.com>
2019-12-02 16:56:43 +0200

    Updates

---
+++ precache-manifest.4c775530b34e330a7425ed197abcfe59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ab78b1bd1eada675bcf51b9cbccaf9",
+    "url": "/WizardLoginization/index.html"
+  },
+  {
+    "revision": "263a08f01963b26c7150",
+    "url": "/WizardLoginization/static/css/main.ffa040fa.chunk.css"
+  },
+  {
+    "revision": "1fef536724a04f763123",
+    "url": "/WizardLoginization/static/js/2.56a8de8a.chunk.js"
+  },
+  {
+    "revision": "263a08f01963b26c7150",
+    "url": "/WizardLoginization/static/js/main.d9c92b09.chunk.js"
+  },
+  {
+    "revision": "55063d27801333c989aa",
+    "url": "/WizardLoginization/static/js/runtime~main.a2392525.js"
+  }
+]);
